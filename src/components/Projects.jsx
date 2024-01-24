de45b0c92a17dafc33e1933deed6dfe05dc27b74
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-01-25 03:14:33 +0500

    Updated some projects' descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,9 +13,9 @@ const Projects = () => {
             code: "https://github.com/shahroz769/link-sharing-app",
             techs: "REACT NODE EXPRESS FRAMER-MOTION",
             shortDescription:
-                "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+                "Create, organize, and share multiple links with anyone hassle-free. Drag-and-drop support to reorder links, mobile previews, and profile customization for a personalized experience.",
             longDescription:
-                "Pain.app is a real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. The tech stack is based on top of Flutter for the mobile app, connected to a Python & FastAPI backend, with data stored in Postgres, deployed on Heroku. Because this isn't real, here's some gibberish to fill space :) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quia officia odio nulla consectetur aperiam ad tempora magni magnam nesciunt. Fuga id sapiente facere ipsa eius exercitationem officiis deleniti, rerum dolorum. Deserunt soluta modi culpa animi.",
+                "Link-sharing app is a platform enabling users to create, organize, and share multiple links with anyone. The app is built using React for frontend, and Node.js, Express.js, and MongoDB on the backend. Users can see mobile mockup previews while creating links. It features drag-and-drop functionality for link reordering, validation checks for link submissions, and the ability to customize profiles with details like pictures, names, and emails. Frame motion is used to smoothly animate between pages.",
         },
         {
             title: "INVOICE WEB APP",
@@ -24,9 +24,9 @@ const Projects = () => {
             code: "https://github.com/shahroz769/invoice-app",
             techs: "REACT NODE EXPRESS FRAMER-MOTION",
             shortDescription:
-                "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+                "Effortlessly manage invoices. Create, edit, search, and filter invoices by status. Save drafts and mark pending invoices as paid.",
             longDescription:
-                "Pain.app is a real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. The tech stack is based on top of Flutter for the mobile app, connected to a Python & FastAPI backend, with data stored in Postgres, deployed on Heroku. Because this isn't real, here's some gibberish to fill space :) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quia officia odio nulla consectetur aperiam ad tempora magni magnam nesciunt. Fuga id sapiente facere ipsa eius exercitationem officiis deleniti, rerum dolorum. Deserunt soluta modi culpa animi.",
+                "Invoice app is a full-stack app made for seamless invoice management. With React on frontend and backend using Node.js, Express.js, and MongoDB, users can efficiently create, edit, and delete invoices. Framer Motion animations enhance the user experience with smooth animations. Users can save invoices as drafts, mark pending invoices as paid, and filter invoices based on status (draft/pending/paid) or search invoice by invoice number. Form validations ensure accuracy during invoice creation and editing. ",
         },
         {
             title: "TIC TAC TOE GAME",
@@ -35,9 +35,9 @@ const Projects = () => {
             code: "https://github.com/shahroz769/tic-tac-toe",
             techs: "HTML CSS JAVASCRIPT GSAP",
             shortDescription:
-                "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+                "A classic 2-player Tic Tac Toe game. Choose your mark, track wins and ties, and experience enhanced gameplay with smooth animations.",
             longDescription:
-                "Pain.app is a real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. The tech stack is based on top of Flutter for the mobile app, connected to a Python & FastAPI backend, with data stored in Postgres, deployed on Heroku. Because this isn't real, here's some gibberish to fill space :) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quia officia odio nulla consectetur aperiam ad tempora magni magnam nesciunt. Fuga id sapiente facere ipsa eius exercitationem officiis deleniti, rerum dolorum. Deserunt soluta modi culpa animi.",
+                "A 2-player Tic Tac Toe game built using HTML, CSS, and JavaScript, offering a classic gaming experience. Players can choose their preferred marks, engage in matches, and keep track of victories and ties through a counter. Animations are incorporated using the GSAP JavaScript Library, bringing engaging dimension to the user interface.",
         },
         {
             title: "DICTIONARY WEB APP",
@@ -46,9 +46,9 @@ const Projects = () => {
             code: "https://github.com/shahroz769/dictionary-web-app",
             techs: "HTML CSS JAVASCRIPT",
             shortDescription:
-                "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+                "Explore the world of words with this Dictionary App. Search, listen, and customize your experience with font choices and light/dark modes.",
             longDescription:
-                "Pain.app is a real-time coaching app for students learning to paint. This app is my baby, designed and built on my own. The tech stack is based on top of Flutter for the mobile app, connected to a Python & FastAPI backend, with data stored in Postgres, deployed on Heroku. Because this isn't real, here's some gibberish to fill space :) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur quia officia odio nulla consectetur aperiam ad tempora magni magnam nesciunt. Fuga id sapiente facere ipsa eius exercitationem officiis deleniti, rerum dolorum. Deserunt soluta modi culpa animi.",
+                "Dictionary Web App is a comprehensive tool for word enthusiasts. Using the Free Dictionary API, users can seamlessly search for words and access detailed responses. Enrich your vocabulary by listening to word pronunciations when available. Reading experience can be customized by switching between serif, sans serif, and monospace fonts or by switching to preferred color theme (light/dark). The app ensures an optimal layout, adapting seamlessly to your device's screen size.",
         },
         {
             title: "DEV FINDER WEB APP",
